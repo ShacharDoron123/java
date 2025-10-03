f54908936c5cc2dcc234b73d208ffdfc307bbe6c
Shachar Doron <shachar08.doron@gmail.com>
2025-10-03 15:04:18 +0300

    Update Main.java

--- tic-tac-toe/Main.java
+++ tic-tac-toe/Main.java
@@ -1,1 +1,11 @@
 
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Tournament tournament = new Tournament(10);
+        tournament.startTournament();
+        tournament.printResults();
+    }
+}
